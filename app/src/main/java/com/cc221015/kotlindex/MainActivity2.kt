354f3d6baf5b4c7e934c1e1ceb2edf71342ed1d2
dekisenpaitm <dekisenpaitm@gmail.com>
2023-10-17 21:43:39 +0200

    Implementing first part of the navigation bar vid: 1:54:29

--- app/src/main/java/com/cc221015/kotlindex/MainActivity2.kt
+++ app/src/main/java/com/cc221015/kotlindex/MainActivity2.kt
@@ -15,7 +15,15 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -30,6 +38,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.cc221015.kotlindex.ui.theme.KotlinDexTheme
 
 class MainActivity2 : ComponentActivity() {
@@ -44,6 +56,7 @@ class MainActivity2 : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     SetBackground()
+                    Navigation()
                     InfoScreen(userName)
                 }
             }
@@ -96,6 +109,43 @@ fun TextBox(text: String, size: Int, trans: Boolean) {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Navigation(){
+    val navController = rememberNavController()
+    Scaffold (bottomBar = {BottomNavigationBar(navController)}
+    ){
+        NavHost(
+            navController = navController,
+            modifier = Modifier.padding(it),
+            startDestination = "first"
+        ){
+        composable("first"){ Text(text="first")}
+        composable("second"){ Text(text="second")}
+        }
+
+    }
+}
+
+@Composable
+fun BottomNavigationBar(navController:NavHostController){
+    BottomNavigation {
+        NavigationBarItem(selected = true,
+            onClick = { navController.navigate("first")},
+            label ={ Text(text ="Home")},
+            icon = { Icon(imageVector = Icons.Default.Home, contentDescription="") })
+
+        NavigationBarItem(selected = false,
+            onClick = {navController.navigate("second")},
+            label ={ Text(text ="Second")},
+            icon = { Icon(imageVector = Icons.Default.AccountBox, contentDescription="") })
+    }
+}
+
+sealed class Screen(route: String){
+    object First: Screen("first")
+    object Second: Screen("second")
+}
 
 
 
